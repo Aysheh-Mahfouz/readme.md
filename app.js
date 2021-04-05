27d1f9ddb6d10b659a4688632d21e64842c85ac3
Aysheh Mahfouz <ayoosh.dub@hotmail.com>
2021-04-05 07:18:51 +0300

    adding java

---
+++ app.js
@@ -0,0 +1,11 @@
+alert
+("welcome here")
+
+var q1= prompt("have you been to jordan before? ,yes/ no ")
+console.log("question #1")
+
+var q2= prompt("have you tried mansaf before?")
+console.log("question #2")
+
+var q3= prompt("from 1 to 10 , how likely to recommend jordanian food to friends?")
+console.log("10 is very , 1 is not likely")

6a9d8649cc2126fde77f9dc16504e85ad253d1bb
Aysheh Mahfouz <ayoosh.dub@hotmail.com>
2021-04-07 21:04:58 +0300

    editing

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 alert
 ("welcome here")
 
-var q1= prompt("have you been to jordan before? ,yes/ no ")
+let q1= prompt("have you been to jordan before? ,yes/ no ")
 console.log("question #1")
+alert('welcome anytime');
 
-var q2= prompt("have you tried mansaf before?")
+let q2= prompt("have you tried mansaf before?")
 console.log("question #2")
+alert('join us at any given time')
 
-var q3= prompt("from 1 to 10 , how likely to recommend jordanian food to friends?")
+let q3= prompt("from 1 to 10 , how likely to recommend jordanian food to friends?")
 console.log("10 is very , 1 is not likely")
+alert('thnx for taking the time to visit my page');
